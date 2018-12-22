e23f1b932f98c6dfca8039478d9cb7e8b989d998
SaraQian <abigail830@163.com>
2018-12-23 00:11:00 +0800

    update mock server

--- src/main/java/com/github/abigail830/mockserver/MockServerApplication.java
+++ src/main/java/com/github/abigail830/mockserver/MockServerApplication.java
@@ -2,10 +2,11 @@ package com.github.abigail830.mockserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.cloud.contract.stubrunner.server.EnableStubRunnerServer;
 
 @SpringBootApplication
-@AutoConfigureWireMock
+//@AutoConfigureWireMock
+@EnableStubRunnerServer
 public class MockServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MockServerApplication.class, args);
